30a7411c1df3806f41f5ec71f1fe577cbb87d54e
Hoàng Cao Minh <hoangcaominh1812@gmail.com>
2026-09-09 22:47:00 +1000

    test: add th07 tests

---
+++ tests/gtest/test_th07.cpp
@@ -0,0 +1,53 @@
+#include "gtest/gtest.h"
+#include "helper.h"
+#include "config.h"
+#include <cstring>
+
+namespace {
+
+class TestTh07 : public TestRpy {};
+
+const char* REPLAYS[] = {
+    "th7_01",
+    "th7_02",
+    "th7_03",
+};
+const size_t REPLAYS_SIZE = sizeof(REPLAYS) / sizeof(REPLAYS[0]);
+
+TEST_F(TestTh07, DetectTh07) {
+    for (size_t i = 0; i < REPLAYS_SIZE; i++) {
+        snprintf(path, sizeof(path), PROJECT_DIR "/samples/th07/%s.rpy", REPLAYS[i]);
+        rpybuf_read(buf, path);
+        EXPECT_EQ(rpybuf_detect(buf), TH07);
+    }
+}
+
+TEST_F(TestTh07, UnpackTh07) {
+    rpy_th07(rpy);
+    for (size_t i = 0; i < REPLAYS_SIZE; i++) {
+        snprintf(path, sizeof(path), PROJECT_DIR "/samples/th07/%s.rpy", REPLAYS[i]);
+        rpybuf_read(buf, path);
+        snprintf(path, sizeof(path), PROJECT_DIR "/samples/th07/%s.rpx", REPLAYS[i]);
+        rpybuf_read(expected_buf, path);
+        rpy_unpack(rpy, buf, buf);
+        EXPECT_EQ(buf->size, expected_buf->size);
+        EXPECT_TRUE(memcmp(buf->data, expected_buf->data, buf->size) == 0);
+        EXPECT_EQ(rpybuf_detect(buf), TH07);
+    }
+}
+
+TEST_F(TestTh07, PackTh07) {
+    rpy_th07(rpy);
+    for (size_t i = 0; i < REPLAYS_SIZE; i++) {
+        snprintf(path, sizeof(path), PROJECT_DIR "/samples/th07/%s.rpx", REPLAYS[i]);
+        rpybuf_read(buf, path);
+        snprintf(path, sizeof(path), PROJECT_DIR "/samples/th07/%s.rpy", REPLAYS[i]);
+        rpybuf_read(expected_buf, path);
+        rpy_pack(rpy, buf, buf);
+        EXPECT_EQ(buf->size, expected_buf->size);
+        EXPECT_TRUE(memcmp(buf->data, expected_buf->data, buf->size) == 0);
+        EXPECT_EQ(rpybuf_detect(buf), TH07);
+    }
+}
+
+}
